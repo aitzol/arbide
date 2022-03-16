844534644898119be45f79c4b1f28e8c8a6244d5
Aitzol Naberan Burgaña <anaberan@gmail.com>
2022-03-16 16:14:02 +0100

    Refactor using routes

--- src/pages/kontaktua.js
+++ src/pages/kontaktua.js
@@ -7,24 +7,47 @@ import ContactForm from '../components/ContactForm';
 
 import SocialLinks from '../components/SocialLinks';
 
-import bgRokodromoa from '../assets/images/rokodromoa.jpg';
 import bgRokodromoa1 from '../assets/images/rokodromoa1.jpeg';
 
 const KontaktuaPage = () => (
   <Layout>
     <Header />
-    <header className="normhead">
+    <header className="masthead">
       <div className="container d-flex h-100 align-items-center">
         <div className="mx-auto text-center">
-          <h1 className="mx-auto my-0 text-uppercase">Gurekin harremanetan jartzeko</h1>
+          <h1 className="mx-auto my-0 text-uppercase">Kontaktua</h1>
         </div>
       </div>
     </header>
-    <section id="erropak" className="projects-section">
+    <section id="kontaktua" className="projects-section">
+      <div className="container">
+        <div className="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div className="col-lg-6">
+            <img className="img-fluid" src={bgRokodromoa1} alt="" />
+          </div>
+          <div className="col-lg-6">
+            <div className="bg-black text-center h-100 project">
+              <div className="d-flex h-100">
+                <div className="project-text w-100 my-auto text-center text-lg-left">
+                  <h4 className="text-white">Idatziguzu</h4>
+                  <p className="mb-0 text-white-50">
+                  arbieskalada@gmail.com
+                  </p>
+                  <hr className="d-none d-lg-block mb-0 ml-0" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+    </section>
+    {/*}
+    <section id="kontaktua" className="projects-section">
       <div className="container">
        <ContactForm />
       </div>
     </section>
+*/}
     <SocialLinks />
     <Footer />
     </Layout>
--- src/pages/rokodromoa.js
+++ src/pages/rokodromoa.js
@@ -13,11 +13,10 @@ import bgRokodromoa1 from '../assets/images/rokodromoa1.jpeg';
 const RokodromoaPage = () => (
   <Layout>
     <Header />
-    <header className="normhead">
+    <header className="masthead">
       <div className="container d-flex h-100 align-items-center">
         <div className="mx-auto text-center">
           <h1 className="mx-auto my-0 text-uppercase">Rokodromoa</h1>
-
         </div>
       </div>
     </header>
