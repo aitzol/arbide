1adc6878a5daa1ae0078109543bcb999df422906
Aitzol Naberan Burgaña <anaberan@gmail.com>
2022-03-19 18:51:21 +0100

    Show OK message

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Alert  from 'react-bootstrap/Alert';
 import ReCAPTCHA from "react-google-recaptcha";
 
 
@@ -14,6 +15,7 @@ export default class EskariaForm extends React.Component {
         emaila: "",
         mezua: "",
         disableSubmit: true,
+        sendOK: false,
       };
     }
     
@@ -37,24 +39,28 @@ export default class EskariaForm extends React.Component {
             emaila: this.state.emaila,
             mezua: this.state.mezua,
           })})
-          let resJson = await res.json();
-          if (res.status === 200) {
-              console.log(resJson);
-          } else {
-              console.log("Some error occured");
-          }
+          this.setState({sendOK: true});
         } catch (err) {
           console.log(err);
         }
       };
-    setDis
+
     render() {
         return (
+          
           <Form onSubmit={this.handleSubmit}>
+            <Alert show={this.state.sendOK} variant="success" style={{ width: "42rem" }}>
+              <Alert.Heading>
+              Eskerrik asko, ahal bezain azkar emango dizugu erantzuna
+              </Alert.Heading>
+            </Alert>
+            {!this.state.sendOK &&
             <Form.Group className="mb-3" controlId="izena" name="izena">
               <Form.Label>Izena</Form.Label>
               <Form.Control placeholder="Zure izena" onChange={this.handleInputChange}/>
             </Form.Group>
+            }
+            {!this.state.sendOK &&
             <Form.Group className="mb-3" controlId="emaila" name="emaila">
               <Form.Label>Eposta helbidea</Form.Label>
               <Form.Control type="email" name="mezua" placeholder="Zure eposta helbidea" onChange={this.handleInputChange} />
@@ -62,17 +68,23 @@ export default class EskariaForm extends React.Component {
                 Erantzunen bat eman ahal izateko baino ez dugu erabiliko
               </Form.Text>
             </Form.Group>
+            }
+            {!this.state.sendOK &&
             <Form.Group className="mb-3" controlId="mezua">
               <Form.Label>Mezua</Form.Label>
               <Form.Control as="textarea" placeholder="" onChange={this.handleInputChange}/>
             </Form.Group>
+            }
+            {!this.state.sendOK &&
             <ReCAPTCHA
               ref={this.recaptchaRef}
               sitekey="6LcSFe4eAAAAAFXjalt3DIIeL9kdfP7o7TSv3oNq"
               onChange={() => this.setState({disableSubmit: false})}
             />
-            
+            }
+            {!this.state.sendOK && 
             <Button type="submit" disabled={this.state.disableSubmit}>Bidali</Button>
+            }
           </Form>
         )
       }
