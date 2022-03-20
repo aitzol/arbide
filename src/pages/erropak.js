6578f98c020ae6adad005da815b5248845e0d953
Aitzol Naberan Burgaña <anaberan@gmail.com>
2022-03-20 16:16:51 +0100

    Erropak first release

---
+++ src/pages/erropak.js
@@ -0,0 +1,138 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import SocialLinks from '../components/SocialLinks';
+
+import sudUrdina from '../assets/images/erropak/sud_urdina.png';
+import sudGrise from '../assets/images/erropak/sud_grise.png';
+import sudBaltza from '../assets/images/erropak/sud_baltza.png';
+
+import kamBaltza from '../assets/images/erropak/kam_baltza.png';
+import kamGrise from '../assets/images/erropak/kam_grise.png';
+import kamZurixe from '../assets/images/erropak/kam_zurixe.png';
+
+const RokodromoaPage = () => (
+  <Layout>
+    <Header />
+    <header className="masthead">
+      <div className="container d-flex h-100 align-items-center">
+        <div className="mx-auto text-center">
+          <h1 className="mx-auto my-0 text-uppercase">Erropak</h1>
+        </div>
+      </div>
+    </header>
+    <section id="rokodromoa" className="projects-section">
+      <div className="container">
+        {/*
+        <div className="row align-items-center no-gutters mb-4 mb-lg-5">
+          <div className="col-xl-8 col-lg-7">
+            <img className="img-fluid mb-3 mb-lg-0" src={bgRokodromoa} alt="" />
+          </div>
+          <div className="col-xl-4 col-lg-5">
+            <div className="featured-text text-center text-lg-left">
+              <h4>Rokodromoa</h4>
+              <p className="text-black-50 mb-0">
+                Taldelanean eraikitzen ari gara tamaina handiko rokodromo bat. 
+                Bertan eskainiko dira...
+              </p>
+            </div>
+          </div>
+        </div>
+        */}
+        <div className="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div className="col-lg-6">
+            <img className="img-fluid" src={sudUrdina} alt="" />
+          </div>
+          <div className="col-lg-6">
+            <div className="bg-black text-center h-100 project">
+              <div className="d-flex h-100">
+                <div className="project-text w-100 my-auto text-center text-lg-left">
+                  <h4 className="text-white">Sudaderak</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      
+        <div className="row justify-content-center no-gutters">
+          <div className="col-lg-6">
+            <img className="img-fluid" src={sudBaltza} alt="" />
+          </div>
+          <div className="col-lg-6 order-lg-first">
+            <div className="bg-black text-center h-100 project">
+              <div className="d-flex h-100">
+                <div className="project-text w-100 my-auto text-center text-lg-right">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div className="col-lg-6">
+            <img className="img-fluid" src={sudGrise} alt="" />
+          </div>
+          <div className="col-lg-6">
+            <div className="bg-black text-center h-100 project">
+              <div className="d-flex h-100">
+                <div className="project-text w-100 my-auto text-center text-lg-left">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      
+        <div className="row justify-content-center no-gutters">
+          <div className="col-lg-6">
+            <img className="img-fluid" src={kamZurixe} alt="" />
+          </div>
+          <div className="col-lg-6 order-lg-first">
+            <div className="bg-black text-center h-100 project">
+              <div className="d-flex h-100">
+                <div className="project-text w-100 my-auto text-center text-lg-right">
+                <h4 className="text-white">Kamisetak</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div className="col-lg-6">
+            <img className="img-fluid" src={kamGrise} alt="" />
+          </div>
+          <div className="col-lg-6">
+            <div className="bg-black text-center h-100 project">
+              <div className="d-flex h-100">
+                <div className="project-text w-100 my-auto text-center text-lg-left">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row justify-content-center no-gutters">
+          <div className="col-lg-6">
+            <img className="img-fluid" src={kamBaltza} alt="" />
+          </div>
+          <div className="col-lg-6 order-lg-first">
+            <div className="bg-black text-center h-100 project">
+              <div className="d-flex h-100">
+                <div className="project-text w-100 my-auto text-center text-lg-right">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <SocialLinks />
+    <Footer />
+    </Layout>
+);
+export default RokodromoaPage;
