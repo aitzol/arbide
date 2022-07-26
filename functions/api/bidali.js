ae3bd2e8336a35cee2b7b8b77bbba9b186e4fec7
Aitzol Naberan Burgaña <anaberan@gmail.com>
2022-07-27 00:22:02 +0200

    Logging

--- functions/api/bidali.js
+++ functions/api/bidali.js
@@ -31,7 +31,7 @@ export async function onRequestPost(context) {
               })
             })
         
-        return new Response(`Mezua bidali da ${res.status_code}`, {
+        return new Response(`Mezua bidali da ${res.text()}`, {
           headers: {
             'Content-Type': 'application/json;charset=utf-8',
           },
