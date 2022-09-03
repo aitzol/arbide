cd1adaf05f4867307b03bcdaadc81d3a50dd197b
Aitzol Naberan Burgaña <anaberan@gmail.com>
2022-09-03 09:05:59 +0200

    Remove contact form

--- src/pages/kontaktua.js
+++ src/pages/kontaktua.js
@@ -3,20 +3,13 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import ContactForm from '../components/ContactForm';
 
 import SocialLinks from '../components/SocialLinks';
 import kontaktua from '../assets/images/eskalada.jpg';
 const KontaktuaPage = () => (
   <Layout>
     <Header />
-    <header className="masthead">
-      <div className="container d-flex h-100 align-items-center">
-        <div className="mx-auto text-center">
-          <h1 className="mx-auto my-0 text-uppercase">Kontaktua</h1>
-        </div>
-      </div>
-    </header>
+    
     <section id="kontaktua" className="projects-section">
       <div class="container">
       <div className="row align-items-center no-gutters mb-4 mb-lg-5">
@@ -27,7 +20,7 @@ const KontaktuaPage = () => (
             <div className="featured-text text-center text-lg-left">
               <h4>Kontaktua</h4>
               <p className="text-black-50 mb-0">
-              <ContactForm />
+              Gurekin kontaktuan jartzeko idatzi <a href="mailto://arbideskalada@gmail.com">arbideskalada@gmail.com</a>"
               </p>
             </div>
           </div>
