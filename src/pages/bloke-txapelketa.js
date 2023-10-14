5dcb60397118ef49f6ab824af621607ad34cf1f2
Aitzol Naberan Burgaña <anaberan@gmail.com>
2023-10-15 00:01:53 +0200

    Gazte txapelketa herrikoia

--- src/pages/bloke-txapelketa.js
+++ src/pages/bloke-txapelketa.js
@@ -7,6 +7,7 @@ import Footer from '../components/Footer';
 import SocialLinks from '../components/SocialLinks';
 
 import kartela from '../assets/images/bloke-txapelketa-2.jpeg';
+import gaztekartela from '../assets/images/gazte-kartela.jpeg';
 
 const RokodromoaPage = () => (
   <Layout>
@@ -15,21 +16,48 @@ const RokodromoaPage = () => (
     <section id="bloke-txapelketa" className="projects-section">
       <div className="container">
         <div className="row justify-content-center no-gutters mb-5 mb-lg-0">
-          <h2>II. Bloke txapelketa herrikoia</h2>
+          <h2>Bloke txapelketa herrikoiak</h2>
+        </div>
+        <div className="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div className="col-lg-6">
+          <a href="https://labur.eus/arbide-2-bloke-txapelketa">
+            <img className="img-fluid" src={kartela} alt="2. bloke txapelketako izen emate kartela" />
+            </a>
+          </div>
+          <div className="col-lg-6">
+            <div className="bg-white text-center h-100 project">
+              <div className="d-flex h-100">
+                <div className="project-text w-100 my-auto text-center text-lg-left">
+                <h3>Nagusien II. Bloke txapelketa herrikoia</h3>
           <p>2023ko urriak 29an ospatuko da Arbideko bigarren bloke txapelketa herrikoia. <a href="https://labur.eus/arbide-2-bloke-txapelketa">Izen ematea</a> urriak 
             17 bitarte zabalik egongo da.
           </p>
-          
-        </div>
-        
-        <div className="row justify-content-center no-gutters mb-5 mb-lg-0">
-          <div className="col-lg-6">
-            <a href="https://labur.eus/arbide-2-bloke-txapelketa">
-            <img className="img-fluid" src={kartela} alt="2. bloke txapelketako izen emate kartela" />
-            </a>  
+                </div>
+              </div>
+            </div>
           </div>
-  
         </div>
+      
+        <div className="row justify-content-center no-gutters">
+          <div className="col-lg-6">
+            <img className="img-fluid" src={gaztekartela} alt="" />
+          </div>
+          <div className="col-lg-6 order-lg-first">
+            <div className="bg-white text-center h-100 project">
+              <div className="d-flex h-100">
+                <div className="project-text w-100 my-auto text-center text-lg-right">
+                <h3>Gazteen I. Bloke txapelketa herrikoia</h3>
+              <p>2023ko azaroak 11an ospatuko da Arbideko lehen gazte bloke txapelketa herrikoia. <a href="https://labur.eus/gazteen-bloke-txapelketa">Izen ematea</a> urriak 
+            30 bitarte zabalik egongo da.
+          </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+ 
 
       </div>
 
