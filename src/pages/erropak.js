c194facd6d670709eada95cd5ae54c41a6d5ab12
Aitzol Naberan Burgaña <anaberan@gmail.com>
2024-05-20 12:27:30 +0200

    Erropak

--- src/pages/erropak.js
+++ src/pages/erropak.js
@@ -14,19 +14,14 @@ const RokodromoaPage = () => (
     <section id="rokodromoa" className="projects-section">
       <div className="container">
         <div className="row justify-content-center no-gutters mb-5 mb-lg-0">
-          <h2>Erropak</h2>
+          <h2>Erropak allariz</h2>
           <p>Pediduk preparaten gabiz eta datorren martizena eta eguenin  (maiatzk 21 eta 23) 17:30 - 18:30</p>
           <p>lokalin eongo ga jasoteko</p>          
           <p>
-            <ol>
-              <li>Edozein zalantza baakazue idatzi <a href="mailto:arbideskalada@gmail.com">arbideskalada@gmail.com</a> eta ahal bezain bixkor erantzungorou (pazientzixa bixkat mesedez :) )</li>
-            </ol>
+            Edozein zalantza baakazue idatzi <a href="mailto:arbideskalada@gmail.com">arbideskalada@gmail.com</a> eta ahal bezain bixkor erantzungorou
           </p>
         </div>
       </div>
-
-      
-    
     </section>
     <SocialLinks />
     <Footer />
