d9644d8fcd0a646a357ce2ff9e0e94e47eb8ef05
Aitzol Naberan Burgaña <anaberan@gmail.com>
2024-05-20 12:38:14 +0200

    Arbide eguna

--- src/pages/bloke-txapelketa.js
+++ src/pages/bloke-txapelketa.js
@@ -6,7 +6,7 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import SocialLinks from '../components/SocialLinks';
 
-import kartela from '../assets/images/bloke-txapelketa-2.jpeg';
+import kartela from '../assets/images/arb_egu.pnh';
 import gaztekartela from '../assets/images/gazte-kartela.jpeg';
 
 const RokodromoaPage = () => (
@@ -16,22 +16,30 @@ const RokodromoaPage = () => (
     <section id="bloke-txapelketa" className="projects-section">
       <div className="container">
         <div className="row justify-content-center no-gutters mb-5 mb-lg-0">
-          <h2>Bloke txapelketa herrikoiak</h2>
+          <h2>Arbide eguna</h2>
         </div>
         <div className="row justify-content-center no-gutters mb-5 mb-lg-0">
           <div className="col-lg-6">
-          <a href="https://labur.eus/arbide-2-bloke-txapelketa">
             <img className="img-fluid" src={kartela} alt="2. bloke txapelketako izen emate kartela" />
-            </a>
           </div>
           <div className="col-lg-6">
             <div className="bg-white text-center h-100 project">
               <div className="d-flex h-100">
                 <div className="project-text w-100 my-auto text-center text-lg-left">
-                <h3>Nagusien II. Bloke txapelketa herrikoia</h3>
-          <p>2023ko urriak 29an ospatuko da Arbideko bigarren bloke txapelketa herrikoia. <a href="https://labur.eus/arbide-2-bloke-txapelketa">Izen ematea</a> urriak 
-            17 bitarte zabalik egongo da.
-          </p>
+                <h3>Egitaraua</h3>
+                <p><h4>Latxanbre enparantzan 10:00 - 14:00</h4>
+                <ul>
+                  <li>Zuhaitz parki</li>
+                  <li>Rokodromu</li>
+                  <li>Txosni</li>
+                  </ul>
+                </p>
+                <p><h4>Zubi zaharrin 17:00</h4>
+                  <ul>
+                    <li>Psikoblock txapeketi</li>
+                    <li>apuntateko <a mailto="arbideskalada@gmail.com">arbideskalada@gmail.com</a> helbidea idatzi eta izen abizena eta edadi adierazi </li>
+                  </ul>
+                </p>
                 </div>
               </div>
             </div>
